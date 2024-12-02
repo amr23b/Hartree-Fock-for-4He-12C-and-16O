3746a41f37e6bd62fc96a5825b22eb784c3c2caf
amr23b <adam.m.ring@gmail.com>
2024-12-02 15:54:15 -0500

    Add files via upload

--- TwoParticleStates.py
+++ TwoParticleStates.py
@@ -61,23 +61,24 @@ while i<Nnucleons:
 #print(f"number of 2 particle states: {NTwoParticleStates}")
 
 #Create Single Particle Hamiltonian
-SingleParticleHamiltonian = np.zeros([NTwoParticleStates,NTwoParticleStates])
+SingleParticleHamiltonian = np.zeros([Nnucleons,Nnucleons])
 i=0
-while i<NTwoParticleStates:
-    SingleParticleHamiltonian[i][i]=(2*TwoParticleStates[i][1][0]+TwoParticleStates[i][1][1]+1.5)*hw+(2*TwoParticleStates[i][0][0]+TwoParticleStates[i][0][1]+1.5)*hw
+while i<Nnucleons:
+    SingleParticleHamiltonian[i][i]=(2*States[i][0]+States[i][1]+1.5)*hw
     i+=1
 #print(SingleParticleHamiltonian)
 
 #Creates Random Density Matrix p
 i=0
-p = 2*np.random.random_sample((NTwoParticleStates,NTwoParticleStates))-1
+p = 2*np.random.random_sample((Nnucleons,Nnucleons))-1
+#p = np.identity(Nnucleons)
 #print(p)
 
 #Normalize initial density matrix
-while i<NTwoParticleStates:
+while i<Nnucleons:
     norm_sqd = 0.
     j=0
-    while j<NTwoParticleStates:
+    while j<Nnucleons:
         norm_sqd+=math.pow(p[i][j],2)
         j+=1
     p[i] = p[i]/math.sqrt(norm_sqd)
@@ -91,31 +92,91 @@ while i<NTwoParticleStates:
 #V_as = (V_mat+V_Transpose)/2
 #print(V_as)
 
-#Creates Antisymmetric V matrix
-V_mat = 2*np.random.random_sample((NTwoParticleStates,NTwoParticleStates))-1
-V_Transpose = np.transpose(V_mat)
-V_as = (V_mat+V_Transpose)/2
+#Creates random Antisymmetric V matrix
+##V_mat = 2*np.random.random_sample((NTwoParticleStates,NTwoParticleStates))-1
+#V_as = np.zeros([Nnucleons,NTwoParticleStates])
+#i=0
+#while i<NTwoParticleStates:
+#    j=i+1
+#    while j<NTwoParticleStates:
+#        V_as[i][j]=V_mat[i][j]-V_mat[j][i]
+#        V_as[j][i]=-V_as[i][j]
+#        V_as[i][i]=V_mat[i][i]
+#        j+=1
+#    i+=1
+#print(V_as)
+#print(np.linalg.matmul(p,V_as))
+
+V_as = np.zeros([Nnucleons,Nnucleons,Nnucleons,Nnucleons])
 i=0
-while i<NTwoParticleStates:
-    j=i+1
-    while j<NTwoParticleStates:
-        V_as[i][j]=V_mat[i][j]-V_mat[j][i]
-        V_as[j][i]=-V_as[i][j]
+while i<Nnucleons:
+    j=0
+    while j<Nnucleons:
+        k=i
+        while k<Nnucleons:
+            m=j
+            while m<Nnucleons:
+                if (States[i][1]==States[j][1]) & (States[i][2]==States[j][2]) & ((States[i][3]+States[j][3])==0)&(States[k][1]==States[m][1]) & (States[k][2]==States[m][2]) & ((States[k][3]+States[m][3])==0):
+                    if V_as[i][j][k][m] != 0: 
+                        m+=1
+                        continue
+                    V_as[i][j][k][m] = 2*np.random.random_sample()-1
+                    V_as[j][i][k][m] = -V_as[i][j][k][m]
+                    V_as[j][i][m][k] = V_as[i][j][k][m]
+                    V_as[i][j][m][k] = -V_as[i][j][k][m]
+
+                    V_as[k][m][i][j] = V_as[i][j][k][m]
+                    V_as[m][k][i][j] = -V_as[i][j][k][m]
+                    V_as[k][m][j][i] = -V_as[i][j][k][m]
+                    V_as[m][k][j][i] = V_as[i][j][k][m]
+                m+=1
+            k+=1
         j+=1
     i+=1
-#print(V_as)
-print(np.linalg.matmul(p,V_as))
-n_iter = 1000
+
+
+
+n_iter = 10
 q=0
+eigenvectors = p
 while q<n_iter:
-    U_HF = np.linalg.matmul(p,V_as)
+    i=0
+    U_HF = np.zeros([Nnucleons,Nnucleons])
+    while i<Nnucleons:
+        j=0
+        while j<Nnucleons:
+            k=0
+            Sum=0.0
+            while k<Nnucleons:
+                m=0
+                while m<Nnucleons:
+                    r=0
+                    while r<Nnucleons:
+                        Sum+=eigenvectors[r][k]*eigenvectors[r][m]*V_as[i][k][j][m]
+                        r+=1
+                    m+=1
+                k+=1
+            U_HF[i][j] = Sum
+            j+=1
+        i+=1
+    #print(U_HF)
     H_HF = U_HF+SingleParticleHamiltonian
     eigenvalues, eigenvectors = linalg.eig(H_HF)
-    if q==0:
-        print(eigenvalues)
-    #print(eigenvalues)
-    #print(eigenvectors)
-    p = np.linalg.matmul(np.conjugate(eigenvectors),eigenvectors)
-    #print(p)
+    #p = np.linalg.matmul(eigenvectors,np.conjugate(eigenvectors))
     q+=1
+    print(q)
+    print(eigenvalues)
+    print(eigenvectors[15])
 print(eigenvalues)
+#print(np.linalg.matmul(np.conjugate(np.transpose(eigenvectors)),eigenvectors))
+
+
+#    pij = np.zeros([NTwoParticleStates, NTwoParticleStates])
+#    for i in range(len(eigenvectors)):
+#        for j in range(len(eigenvectors)):
+#            SSum = 0.0
+#            for a in range(NTwoParticleStates):
+#                SSum += eigenvectors[i, a]*eigenvectors[j, a]
+#            pij[i][j] = SSum
+    #print(pij)
+    #print(p)
